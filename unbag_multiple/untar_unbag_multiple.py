20165a95c88a6d6b121c518fbe923cb3b3f11f23
lmenzies <lmenzies@lmenzies>
2023-04-11 17:30:41 -0500

    fix untar_unbag_multiple to keep metadata files

--- unbag_multiple/untar_unbag_multiple.py
+++ unbag_multiple/untar_unbag_multiple.py
@@ -42,13 +42,38 @@ def untar_targz(tarsdir, tarslist):
     print(f'Extracted contents of {counting} tar files with {errs} extraction errors.\n')
     return extractok
 
+def unbag_keep_meta(bag_dir, bag_list):
+    item_count = 0
+    bag_count = 0
+    for b in bag_list:
+        item_count += 1
+        bag_path = path.join(bag_dir, b)
+        data_path = path.join(bag_path, 'data')
+        item_path = path.join(data_path, b)
+        temp_path = path.join(bag_path, b)
+        if path.exists(data_path):
+            bag_count += 1
+            # delete bagit files
+            for f in listdir(bag_path):
+                fpath = path.join(bag_path, f)
+                if path.isfile(fpath):
+                    remove(fpath)
+            # move metadata.csv and manifest.csv up one level
+            for m in listdir(data_path):
+                mpath = path.join(data_path, m)
+                new_mpath = path.join(bag_path, m)
+                if path.isfile(mpath):
+                    move(mpath, new_mpath)
+            # move 'payload' from 'data' folder up one level (keeping the DnD 'prebag' structure)
+            move(item_path, temp_path)
+            rmtree(data_path)
+            # rename(temp_path, bag_path)
+        nums = [item_count, bag_count]
+    return nums
+
 def unbag_dnd_bags(bagdir, bagslist):
     itemcount = 0
     bagcount = 0
-    del_files = ['bag-info.txt', 'bagit.txt', 'aptrust-info.txt', 'manifest-md5.txt',
-                'manifest-sha256.txt', 'manifest-sha512.txt', 'tagmanifest-md5.txt',
-                'tagmanifest-sha256.txt', 'tagmanifest-sha512.txt', 'metadata.csv',
-                'manifest.csv', 'metadata.xml', 'metadata.json']
     for b in bagslist:
         itemcount += 1
         bagpath = path.join(bagdir, b)
@@ -57,16 +82,11 @@ def unbag_dnd_bags(bagdir, bagslist):
         temppath = path.join(bagdir, f'temp{itemcount}')
         if path.exists(datapath):
             bagcount += 1
-            # delete bagit files
+            # delete bagit and metadata files
             for f in listdir(bagpath):
                 fpath = path.join(bagpath, f)
-                if f in del_files:
+                if path.isfile(fpath):
                     remove(fpath)
-            # delete metadata files
-            for m in listdir(datapath):
-                mpath = path.join(datapath, m)
-                if m in del_files:
-                    remove(mpath)
             # move 'payload' up to original file structure
             move(itempath, temppath)
             rmtree(bagpath)
@@ -116,6 +136,13 @@ if yesno in ['Y', 'y', 'Yes', 'yes', '']:
     dnd_bags = True
 else:
     dnd_bags = False
+keep_meta = False
+if dnd_bags == True:
+    askyesno = input(f'Do you want to KEEP the metadata.csv and manifest.csv files? ').strip()
+    if askyesno in ['Y', 'y', 'Yes', 'yes', '']:
+        keep_meta = True
+    else:
+        keep_meta = False
 tarlist = [t for t in listdir(bagsdir) if tarfile.is_tarfile(path.join(bagsdir, t))]
 success = False
 success = untar_targz(bagsdir, tarlist)
@@ -123,8 +150,10 @@ if not success:
     print(f'Tar extraction failed.\nQuitting.')
     exit()
 baglist = [i for i in listdir(bagsdir) if len(i.split('.')) == 1]
-if dnd_bags == True:
-    numbers = unbag_dnd_bags(bagsdir, baglist)
-else:
+if keep_meta == True:
+    numbers = unbag_keep_meta(bagsdir, baglist)
+elif dnd_bags == False:
     numbers = unbag_normal_bags(bagsdir, baglist)
+else:
+    numbers = unbag_dnd_bags(bagsdir, baglist)
 print(f'\nFound {numbers[0]} folders.\nUnbagged {numbers[1]} bags.\n')
