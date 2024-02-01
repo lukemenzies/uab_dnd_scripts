fd2513425015ff86ba77706d7ab0f3ef072b861c
lmenzies <lmenzies@lmenzies>
2024-02-01 09:36:11 -0600

    edit batch file

--- shortcuts/dsl_shortcuts.bat
+++ shortcuts/dsl_shortcuts.bat
@@ -3,8 +3,8 @@
 REM Batch file to create shortcuts for the DnD Shares on a Windows machine
 REM Also checks for Python and if present installs setuptools and modules
 
-SET "dndusername=none"
-SET "dndpassword=none"
 SET /p "dndusername=Enter the User Name:"
 SET /p "dndpassword=Enter the Password:"
-MKLINK "%USERPROFILE%\Map to H Drive (historical)" "C:\WINDOWS\System32\net.exe use H: \\138.26.152.9\historical_staging /user:%dndusername% %dndpassword%"
+
+MKLINK %USERPROFILE%\Desktop\"Map to H Drive (historical)" "C:\WINDOWS\System32\net.exe use H: \\138.26.152.9\historical_staging /user:%dndusername% %dndpassword%"
+
